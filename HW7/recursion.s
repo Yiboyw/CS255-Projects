e6f654c432ff2e6b3bc50b3e36a9a3ae897a9e9d
Yibo Wang <yibowangyw@gmail.com>
2018-04-09 17:18:04 -0400

    Update recursion.s

--- HW7/recursion.s
+++ HW7/recursion.s
@@ -26,6 +26,27 @@ F:
 * Also: Make sure you DE-ALLOCATE the local variables and restore a6
 *       BEFORE you return to the caller !!!
 * ********************************************************************
+
+*int F(int i, int j, int k)
+* {
+*     int s, t;
+*
+*     if ( i <= 0 || j <= 0 )
+*        return(-1);
+*     else if ( i + j < k )
+*        return (i+j);
+*     else
+*     {
+*        s = 0;
+*        for (t = 1; t < k; t++)
+*        {
+*           s = s + F(i-t, j-t, k-1) + 1;               
+*        }
+*        return(s);
+*     }
+*  }
+
+
 *save caller's frame pointer
  move.l a6, -(a7) 
 *setup my own frame pointer

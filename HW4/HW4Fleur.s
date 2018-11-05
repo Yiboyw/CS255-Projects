05551e6fafd1b9aa5b4a30aa2e1feb107d6a6924
Yibo Wang <yibowangyw@gmail.com>
2018-11-05 16:59:47 -0500

    Update HW4Fleur.s

--- HW4/HW4Fleur.s
+++ HW4/HW4Fleur.s
@@ -1,4 +1,10 @@
-*THIS CODE WAS MY OWN WORK. IT WAS WRITTEN WITHOUT CONSULTING CODE WRITTEN BY OTHER STUDENTS _Yibo_Wang_
+* Collaboration Statement:
+* Name: Yibo Wang
+* Email: yibo.wang@emory.edu
+* UserID: ywan738
+* THIS  CODE  WAS MY OWN WORK , IT WAS  WRITTEN  WITHOUT  CONSULTING
+* CODE  WRITTEN  BY  OTHER  STUDENTS. _Yibo_Wang_
+
 
 * ********************************************************************
 * Do not touch the following 2 xdef lists:
@@ -9,7 +15,7 @@
 * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 * You can add more xdef here to export labels to emacsim
 * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	xdef i, j, Q1, Q2, Q3, Q4, Q5, Q6, Q7
+         xdef i, j, Q1, Q2, Q3, Q4, Q5, Q6, Q7
 
 * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 *      Put your assembler program here - between the start and stop label
@@ -17,55 +23,82 @@
 * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Start:
 
-*Find the min for array A 
-	move.l A, min *min = A[0] 
+*Find the max for array A
+	move.l A, max *max = A[0] 
 	move.l #1, i *move i = 1
+        movea.l #A, a0
 
- WhileStart2:
+  WhileStart:
 	move.l i, d1 *d1 = i
 	cmp.l len_a, d1 *compares i to length of A
-	BGE End4 * if greater than or equal to end
+	BGE End1 * if greater than or equal to end
 	
-	movea.l #A, a0
-	move.l i, d1 
 	muls #4, d1 *muls i by 4
 	move.l 0(a0,d1), d3 *d3 = A[i]
 	
-	move.l min, d4 *d4 = min
-	cmp.l d4, d3 *Compare A[i] to min
-	bge IfEnd2 *if A[i] >= min then i++
-	move.l d3, min *min = A[i] 
+	move.l max, d4 *d4 = max
+	cmp.l d4, d3 *Compare A[i] to max
+	ble IfEnd *if A[i] <= max then i++
+	move.l d3, max *else: max = A[i] 
+
+ IfEnd:
+	move.l i, d0
+	add.l #1, d0
+	move.l d0, i
+
+	bra WhileStart
+
+ End1:
+
+
+Q1: 
+
+*Find the max for array B
+ move.l #0, i *i = 0
+ move.l i, d0 *d0 = i = 0
+ movea.l #B, a1 *a1 = B
+
+ WhileStart2:
+ 	 move.l i, d1 
+ 	 cmp.l len_b, d1
+ 	 BGE End2
+ 	 
+	 muls #4, d1
+	 move.l 0(a1, d1), d3 *d3 = B[i]
+         
+         move.l max, d4
+	 cmp.l d4, d3
+         ble IfEnd2
+	 move.l d3, max *max = B[i]
 
  IfEnd2:
 	move.l i, d0
 	add.l #1, d0
 	move.l d0, i
+ 	bra WhileStart2
+ End2:
 
-	bra WhileStart2
+Q2:
 
- End4:
+*Find the min for array A
+	move.l A, min *min = A[0] 
+	move.l #1, i *move i = 1
+        movea.l #A, a0
 
-Q1:
-*Find the min for array B
-	move.l #0, i *move i = 0
-
- WhileStart3:
-	move.l i, d1 * d1 = i
-	cmp.l len_b, d1 *compares i to length of B
+  WhileStart3:
+	move.l i, d1 *d1 = i
+	cmp.l len_a, d1 *compares i to length of A
 	BGE End3 * if greater than or equal to end
 	
-	movea.l #B, a0
-	move.l i, d1 
 	muls #4, d1 *muls i by 4
 	move.l 0(a0,d1), d3 *d3 = A[i]
 	
-	move.l min, d4 *d1 = min
-	cmp.l d4, d3 *Compare B[i] to min
-	bge IfEnd3 *if B[i] <= min then go to next item
-	move.l d3, min *min = B[i]
+	move.l min, d4 *d4 = min
+	cmp.l d4, d3 *Compare A[i] to min
+	bge IfEnd3 *if A[i] >= min then i++
+	move.l d3, min *min = A[i] 
 
  IfEnd3:
-	*i++
 	move.l i, d0
 	add.l #1, d0
 	move.l d0, i
@@ -74,66 +107,38 @@ Q1:
 
  End3:
 
-Q2:
-*Find the max for array A
-	move.l A, max *max = A[0]
-	move.l #1, i *move i = 0
 
- WhileStart5:
-	move.l i, d1 * d1 = i
-	cmp.l len_a, d1 *compares i to length of A
-	BGE End2 * if greater than or equal to end
-	
-	movea.l #A, a0
-	move.l i, d1 
-	muls #4, d1 *muls i by 4
-	move.l 0(a0,d1), d3 *d3 = A[i]
-	
-	move.l max, d4 *d4 = max
-	cmp.l d4, d3 *Compare A[i] to max
-	ble IfEnd5 *if A[i] <= max then go to next item
-	move.l d3, max *max = A[i]
+Q3: 
 
- IfEnd5:
+*Find the min for array B
+ move.l #0, i *i = 0
+ move.l i, d0 *d0 = i = 0
+ movea.l #B, a1 *a1 = B
+
+ WhileStart4:
+ 	 move.l i, d1 
+ 	 cmp.l len_b, d1
+ 	 BGE End4
+ 	 
+	 muls #4, d1
+	 move.l 0(a1, d1), d3 *d3 = B[i]
+         
+         move.l min, d4
+	 cmp.l d4, d3
+         bge IfEnd4
+	 move.l d3, min *min = B[i]
+
+ IfEnd4:
 	move.l i, d0
 	add.l #1, d0
 	move.l d0, i
+ 	bra WhileStart4
+ End4:
 
-	bra WhileStart5
-
- End2:
-
-Q3:
-*Find the max for Array A + B
-	move.l #0, i *move i = 0
-
- WhileStart:
-	move.l i, d1 * d1 = i
-	cmp.l len_a, d1 *compares i to length of B
-	BGE End5 * if greater than or equal to end
-	
-	movea.l #B, a0
-	move.l i, d1 
-	muls #4, d1 *muls i by 4
-	move.l 0(a0,d1), d3 *d3 = A[i]
-	
-	move.l max, d4 *d4 = max
-	cmp.l d4, d3 *Compare B[i] to max
-	ble IfEnd *if B[i] <= max then go to next item
-	move.l d3, max *max = B[i]
-
- IfEnd:
-	*i++
-	move.l i, d0
-	add.l #1, d0
-	move.l d0, i
-
-	bra WhileStart
-
- End5:
 
 Q4:
-*Find the sum of array A
+ 
+ *Find the sum of array A
 	move.l #0, sum
 	move.l #0, i
 	
@@ -147,8 +152,7 @@ Q4:
 	move.l i, d1
 	muls #4, d1
 	move.l 0(a0, d1), d2 *d2 = A[i]
-	add.l d0, d2 *adds sum + A[i] = d2
-	move.l d2, sum *d2 = sum
+	add.l d2, sum *sum = sum + A[i]
 	
 	*i++
 	move.l i, d0
@@ -157,9 +161,9 @@ Q4:
 	BRA Loop
 	
  LoopExit:
-
+ 
 Q5:
-*Find the sum of array A + B
+*Find the sum of array B
 	move.l #0, i
 	
  Loop2:
@@ -172,8 +176,7 @@ Q5:
 	move.l i, d1 *d1=i
 	muls #4, d1
 	move.l 0(a0, d1), d2 *d2 = A[i]
-	add.l d0, d2 *adds sum + A[i] = d2
-	move.l d2, sum *d2 = sum
+	add.l d2, sum *sum = sum + A[i]
 	
 	*i++
 	move.l i, d0
@@ -184,10 +187,12 @@ Q5:
  LoopExit2:
 
 Q6:
+
 *Find the common elements in A and B
  move.l #0, i
  move.l #0, j
- move.l i, d0 
+ move.l i, d0
+ muls #4, d0 
  move.l j, d1
  move.l #0, common *make common equal to zero
  movea.l #A, a0
@@ -199,6 +204,7 @@ Q6:
  Begin2:	
 	cmp.l len_b, d1
 	BGE End9
+	move.l i, d0
 	muls #4, d0	
 	muls #4, d1	
 	move.l 0(a0, d0), d3 *d3 = A[i]
@@ -219,6 +225,7 @@ Q6:
 
 	bra Begin2
 
+
  End9:
 	*i++
 	move.l i, d0
@@ -233,8 +240,6 @@ Q6:
   End10:
 
 Q7:
-
-	rts
 
 * ********************************************************************
 * Do not touch the stop label - you need it to stop the program
@@ -251,17 +256,19 @@ Stop:   nop
 *
 *    You can add more variables below this line if you need them
 * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-i: dc.l 0
-j: dc.l 0
+ i: dc.l 0
+ j: dc.l 0
+
+
 
 
 * ********************************************************************
 * Adjust the arrays (and lengths) below to test different arrays
 * ********************************************************************
-A:      dc.l  13,3,21,1,8,5,4,23
-B:      dc.l  4,8,15,16,23,42
-len_a:  dc.l  8
-len_b:  dc.l  6
+A:      dc.l  4,5,22,2,1
+B:      dc.l  1,2,9,17,11,16,10
+len_a:  dc.l 5
+len_b:  dc.l 7
 
 * ********************************************************************
 * Do not touch anything below this line !!!
